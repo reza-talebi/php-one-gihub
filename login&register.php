5e9b63f30dea419499d2cfe6d563cb5ebbf40e2b
reza-talebi <reza.talebi0902@gmail.com>
2020-12-21 23:59:56 -0800

    edit names to username

--- login&register.php
+++ login&register.php
@@ -2,93 +2,87 @@
 <html lang="en">
 
 <head>
-    <title></title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="css/sign.css" rel="stylesheet">
+   <title></title>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link href="css/sign.css" rel="stylesheet">
 </head>
 
 <body>
-    <div class="form">
+   <div class="form">
 
-        <ul class="tab-group">
-            <li class="tab active"><a href="#signup">Sign Up</a></li>
-            <li class="tab"><a href="#login">Log In</a></li>
-        </ul>
+      <ul class="tab-group">
+         <li class="tab active"><a href="#signup">Sign Up</a></li>
+         <li class="tab"><a href="#login">Log In</a></li>
+      </ul>
 
-        <div class="tab-content">
-            <div id="signup">
-                <h1>Sign Up for Free</h1>
+      <div class="tab-content">
+         <div id="signup">
+            <h1>Sign Up for Free</h1>
 
-                <form action="/" method="post">
+            <form action="/" method="post">
 
-                    <div class="top-row">
-                        <div class="field-wrap">
-                            <label>
-                                First Name<span class="req">*</span>
-                            </label>
-                            <input type="text" required autocomplete="off" />
-                        </div>
+               <div class="top-row">
+                  <div class="field-wrap">
+                     <label>
+                        UserName<span class="req">*</span>
+                     </label>
+                     <input type="text" required autocomplete="off" />
+                  </div>
+               </div>
 
-                        <div class="field-wrap">
-                            <label>
-                                Last Name<span class="req">*</span>
-                            </label>
-                            <input type="text" required autocomplete="off" />
-                        </div>
-                    </div>
+               <div class="field-wrap">
+                  <label>
+                     Email Address<span class="req">*</span>
+                  </label>
+                  <input type="email" required autocomplete="off" />
+               </div>
 
-                    <div class="field-wrap">
-                        <label>
-                            Email Address<span class="req">*</span>
-                        </label>
-                        <input type="email" required autocomplete="off" />
-                    </div>
+               <div class="field-wrap">
+                  <label>
+                     Set A Password<span class="req">*</span>
+                  </label>
+                  <input type="password" required autocomplete="off" />
+               </div>
 
-                    <div class="field-wrap">
-                        <label>
-                            Set A Password<span class="req">*</span>
-                        </label>
-                        <input type="password" required autocomplete="off" />
-                    </div>
+               <button type="submit" class="button button-block" />Get Started</button>
 
-                    <button type="submit" class="button button-block" />Get Started</button>
+            </form>
 
-                </form>
+         </div>
 
-            </div>
+         <div id="login">
+            <h1>Welcome Back!</h1>
 
-            <div id="login">
-                <h1>Welcome Back!</h1>
+            <form action="/" method="post">
 
-                <form action="/" method="post">
+               <div class="field-wrap">
+                  <label>
+                     Email Address<span class="req">*</span>
+                  </label>
+                  <input type="email" required autocomplete="off" />
+               </div>
 
-                    <div class="field-wrap">
-                        <label>
-                            Email Address<span class="req">*</span>
-                        </label>
-                        <input type="email" required autocomplete="off" />
-                    </div>
+               <div class="field-wrap">
+                  <label>
+                     Password<span class="req">*</span>
+                  </label>
+                  <input type="password" required autocomplete="off" />
+               </div>
 
-                    <div class="field-wrap">
-                        <label>
-                            Password<span class="req">*</span>
-                        </label>
-                        <input type="password" required autocomplete="off" />
-                    </div>
+               <p class="forgot"><a href="#">Forgot Password?</a></p>
 
-                    <p class="forgot"><a href="#">Forgot Password?</a></p>
+               <button class="button button-block" />Log In</button>
 
-                    <button class="button button-block" />Log In</button>
+            </form>
 
-                </form>
+         </div>
 
-            </div>
+      </div><!-- tab-content -->
 
-        </div><!-- tab-content -->
-
-    </div> <!-- /form -->
+   </div> <!-- /form -->
 </body>
 <script src="js/jquery-3.3.1.js"></script>
 <script src="js/sign.js"></script>
+
 </html>
